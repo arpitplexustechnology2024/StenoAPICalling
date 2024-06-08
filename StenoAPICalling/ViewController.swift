ed8487cea33969c9c42826f97a6709c3f53e0046
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-06-07 18:15:32 -0700

    Initial Commit

---
+++ StenoAPICalling/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StenoAPICalling
+//
+//  Created by Arpit iOS Dev. on 07/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StenoAPICallingUITests/StenoAPICallingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  StenoAPICallingUITestsLaunchTests.swift
+//  StenoAPICallingUITests
+//
+//  Created by Arpit iOS Dev. on 07/06/24.
+//
+
+import XCTest
+
+final class StenoAPICallingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
